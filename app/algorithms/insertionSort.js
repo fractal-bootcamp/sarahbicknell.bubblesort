d0aa704d47f7cb9c6b81a16e4cbd1ef1d6dc51e6
sarahbicknell <166096006+sarahbicknell@users.noreply.github.com>
2024-06-25 18:18:22 -0400

    fixed the insertion sort

--- app/algorithms/insertionSort.js
+++ app/algorithms/insertionSort.js
@@ -1,38 +1,83 @@
+// const testArray = [6, 5, 3, 1, 8, 7, 2, 4]
+
+// const outputHistory = []
+
+// function insertionSort(input) {
+//     let output = []
+//     output[0] = input[0]
+//     outputHistory.push({output: output.slice(), currentValue: input[0]});
+
+//     //for each element in the input array , going in order
+//     for (let i = 1; i < input.length; i++){
+//         //assign that element to the current value we're checking 
+//         let currentValue = input[i]
+
+//         let inserted = false;
+//         // for each element in the output array, starting at the end
+//         for (let o = output.length - 1 ; o >= 0; o--){
+//             //check if that value is larger than currentvalue
+//             if (output[o] >= currentValue){
+//                 //if it is , move its index up by one 
+//                 output[o + 1] = output[o]
+//                 //if it isn't, we want to insert the currentvalue right next to it 
+//                 //then we swtich insert to true to we know insertion has happened 
+//             } else {
+//                 output[o + 1] = currentValue
+//                 inserted = true ;
+                
+//                 break
+//             }
+//         }
+//         //if insert hasn't happened we know we're at the end so the value is smaller than anything in output and should be put at start 
+//         if (inserted === false) {
+//             output[0] = currentValue;
+//         }
+//         outputHistory.push({output: output.slice(), currentValue})
+//     } 
+
+// return outputHistory
+
+// }
+
+// console.log(insertionSort(testArray))
+
+//test in place
+
 const testArray = [6, 5, 3, 1, 8, 7, 2, 4]
 
-const outputHistory = []
 
 function insertionSort(input) {
-    let output = []
-    output[0] = input[0]
-    outputHistory.push({output: output.slice(), currentValue: input[0]});
+    const outputHistory = []
+    outputHistory.push({output: input.slice(), currentValue: null});
 
-    //for each element in the input array , going in order
+    //for each element in the input array , going in order, starting with the second one 
     for (let i = 1; i < input.length; i++){
         //assign that element to the current value we're checking 
         let currentValue = input[i]
 
         let inserted = false;
         // for each element in the output array, starting at the end
-        for (let o = output.length - 1 ; o >= 0; o--){
+        for (let o = i - 1 ; o >= 0 ; o--){
             //check if that value is larger than currentvalue
-            if (output[o] >= currentValue){
+            if (input[o] >= currentValue){
                 //if it is , move its index up by one 
-                output[o + 1] = output[o]
+                input[o + 1] = input[o]
                 //if it isn't, we want to insert the currentvalue right next to it 
                 //then we swtich insert to true to we know insertion has happened 
             } else {
-                output[o + 1] = currentValue
+                input[o + 1] = currentValue
                 inserted = true ;
+                outputHistory.push({ output: input.slice(), currentValue }); // Capture state after each shift
+
                 
                 break
             }
         }
         //if insert hasn't happened we know we're at the end so the value is smaller than anything in output and should be put at start 
         if (inserted === false) {
-            output[0] = currentValue;
+            input[0] = currentValue;
+            outputHistory.push({output: input.slice(), currentValue})
         }
-        outputHistory.push({output: output.slice(), currentValue})
     } 
 
 return outputHistory
@@ -49,3 +94,4 @@ export default insertionSort
 //if it's smaller, check it against the previous element at output[i - 1]
 //keep going until an element it's smaller than is found
 //insert it there, and move all the elements that were bigger than it up one index
+
